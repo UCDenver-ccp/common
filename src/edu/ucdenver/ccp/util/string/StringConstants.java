9af321a3f0200062905434af4b142d94c3c6defe
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-03-25 17:00:23 +0000

    Initial commit of a class to store commonly used String constants, e.g. BLANK, NEW_LINE, etc.

---
+++ src/edu/ucdenver/ccp/util/string/StringConstants.java
@@ -0,0 +1,12 @@
+package edu.ucdenver.ccp.util.string;
+
+public class StringConstants {
+	public static final String NEW_LINE = "\n";
+	public static final String BLANK = "";
+	public static final String POUND = "#";
+	public static final String TWO_FORWARD_SLASHES = "//";
+
+	private StringConstants() {
+		// this class should not be instantiated
+	}
+}
