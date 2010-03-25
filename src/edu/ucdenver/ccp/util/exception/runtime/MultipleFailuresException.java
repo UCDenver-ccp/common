d842ad257f2128ee824316be56e20e9aa91e3d47
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-03-25 17:06:11 +0000

    Commit of an Runtime Exception subclass that is used to document cases when multiple exceptions have been thrown.

---
+++ src/edu/ucdenver/ccp/util/exception/runtime/MultipleFailuresException.java
@@ -0,0 +1,24 @@
+package edu.ucdenver.ccp.util.exception.runtime;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MultipleFailuresException extends RuntimeException {
+
+	private final String message;
+	private final List<Throwable> failures;
+
+	public MultipleFailuresException(String message, Throwable... throwables) {
+		this.message = message;
+		failures = Arrays.asList(throwables);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public List<Throwable> getFailures() {
+		return failures;
+	}
+	
+}
