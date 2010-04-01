6fcffec58ebf5050f7ec3ed455fcc59ec7c4eea2
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-04-01 22:39:34 +0000

    Added utility methods for deleting/cleaning directories, as well as a copy() method.

--- src/edu/ucdenver/ccp/util/file/FileUtil.java
+++ src/edu/ucdenver/ccp/util/file/FileUtil.java
@@ -1,7 +1,14 @@
 package edu.ucdenver.ccp.util.file;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
 
 public class FileUtil {
 
@@ -25,6 +32,103 @@ public class FileUtil {
 			errorMessage = String.format("Input directory is not a directory: %s", directory.getAbsolutePath());
 		}
 		return errorMessage;
+	}
+
+	/**
+	 * Checks to see if the directory exists, and if it is truly a directory. Throws a
+	 * FileNotFoundException if the input is not a true/existing directory.
+	 * 
+	 * @param directory
+	 * @throws FileNotFoundException
+	 */
+	public static void validateDirectory(File directory) throws FileNotFoundException {
+		String message = FileUtil.isDirectoryValid(directory);
+		if (message != null) {
+			throw new FileNotFoundException(message);
+		}
+	}
+
+	/**
+	 * Creates the specified directory. Throws an IllegalStateException if the directory cannot be
+	 * created.
+	 * 
+	 * @param directory
+	 * @throws IllegalStateException
+	 */
+	public static void mkdir(File directory) throws IllegalStateException {
+		boolean succeeded = directory.mkdirs();
+		if (!succeeded) {
+			throw new IllegalStateException(String.format("Error while creating directory: %s", directory
+					.getAbsolutePath()));
+		}
+	}
+
+	/**
+	 * Recursively deletes the contents of a directory, and the directory itself
+	 * 
+	 * @param directory
+	 * @return
+	 */
+	public static boolean deleteDirectory(File directory) {
+		if (directory.exists()) {
+			File[] files = directory.listFiles();
+			for (File f : files) {
+				if (f.isDirectory()) {
+					deleteDirectory(f);
+				} else {
+					f.delete();
+				}
+			}
+		}
+		return directory.delete();
+	}
+
+	/**
+	 * Deletes files from a given directory, does not touch directories.
+	 * 
+	 * @param directory
+	 * @return
+	 */
+	public static boolean deleteFilesFromDirectory(File directory) {
+		if (directory.exists()) {
+			for (File f : directory.listFiles()) {
+				if (f.isFile()) {
+					if (!f.delete()) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Deletes a directory (including any contents) and recreates it so that it is empty
+	 * 
+	 * @param directory
+	 * @return
+	 */
+	public static boolean cleanDirectory(File directory) {
+		deleteDirectory(directory);
+		return directory.mkdirs();
+	}
+
+	/**
+	 * Copies the contents of the InputStream to the specified File
+	 * 
+	 * @param is
+	 * @param file
+	 * @throws IOException
+	 */
+	public static void copy(InputStream is, File file) throws IOException {
+		BufferedOutputStream outStream = null;
+		try {
+			outStream = new BufferedOutputStream(new FileOutputStream(file));
+			IOUtils.copyLarge(is, outStream);
+		} finally {
+			IOUtils.closeQuietly(outStream);
+		}
 	}
 
 	/**
