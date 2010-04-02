f550a9451fc68fc7955db1f471b41d1965081a8e
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-04-02 22:18:33 +0000

    Added a TemporaryFolder so that it is available to all subclasses.

--- src/edu/ucdenver/ccp/util/test/DefaultTestCase.java
+++ src/edu/ucdenver/ccp/util/test/DefaultTestCase.java
@@ -3,6 +3,8 @@ package edu.ucdenver.ccp.util.test;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * A super class for other test classes. It sets up a logger among other things.
@@ -17,4 +19,6 @@ public class DefaultTestCase {
 		Logger.getRootLogger().setLevel(Level.DEBUG);
 	}
 	
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
 }
