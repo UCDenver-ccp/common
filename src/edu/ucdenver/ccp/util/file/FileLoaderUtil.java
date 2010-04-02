d5ffc092c2decbdf920ec4df54c179aad8b56af6
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-04-02 18:11:56 +0000

    Added capability to extract arbitrary columns from a file.

--- src/edu/ucdenver/ccp/util/file/FileLoaderUtil.java
+++ src/edu/ucdenver/ccp/util/file/FileLoaderUtil.java
@@ -16,6 +16,73 @@ public class FileLoaderUtil {
 	private static final String FILE_ENCODING_PROPERTY = "file.encoding";
 	private static final String DEFAULT_ENCODING = System.getProperty(FILE_ENCODING_PROPERTY);
 
+	public static List<String[]> loadColumnsFromDelimitedFile(File inputFile, String delimiter,
+			String commentIndicator, int... columnIndexes) throws IOException, ArrayIndexOutOfBoundsException,
+			IllegalArgumentException {
+		return loadColumnsFromDelimitedFile(inputFile, DEFAULT_ENCODING, delimiter, commentIndicator, columnIndexes);
+	}
+
+	/**
+	 * Returns a List<String[]> containing the column values for the input file. One String[] per
+	 * line of the file. If you want the entire line, set the delimiter to be null. If there is a
+	 * delimiter specified, you must specify at least one column index to return.
+	 * 
+	 * @param inputFile
+	 * @param encoding
+	 * @param delimiter
+	 * @param commentIndicator
+	 * @param columnIndexes
+	 * @return
+	 * @throws IOException
+	 * @throws ArrayIndexOutOfBoundsException
+	 *             - if a requested column index does not exist
+	 * @throws IllegalArgumentException
+	 *             - if a delimiter is specified, but no column indexes are requested
+	 */
+	public static List<String[]> loadColumnsFromDelimitedFile(File inputFile, String encoding, String delimiter,
+			String commentIndicator, int... columnIndexes) throws IOException, ArrayIndexOutOfBoundsException,
+			IllegalArgumentException {
+		if (delimiter != null && (columnIndexes == null || columnIndexes.length == 0)) {
+			throw new IllegalArgumentException(String.format(
+					"Cannot parse columns from line. A delimiter \"%s\" has been specified, "
+							+ "however no columns have been requested. If you want the entire line, "
+							+ "set the delimiter to be null.", delimiter));
+		}
+		List<String[]> outputColumns = new ArrayList<String[]>();
+		for (Iterator<String> lineIter = getLineIterator(inputFile, encoding, commentIndicator); lineIter.hasNext();) {
+			String line = lineIter.next();
+			outputColumns.add(getColumnsFromLine(line, delimiter, columnIndexes));
+		}
+		return outputColumns;
+	}
+
+	/**
+	 * Parses the input line and returns a String[] containing the requested columns of that line.
+	 * If the delimiter is null or if there are no column indexes specified, the entire line is
+	 * returned.
+	 * 
+	 * @param line
+	 * @param delimiter
+	 * @param columnIndexes
+	 * @return
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
+	private static String[] getColumnsFromLine(String line, String delimiter, int... columnIndexes)
+			throws ArrayIndexOutOfBoundsException {
+		if (delimiter == null) {
+			return new String[] { line };
+		} else {
+			String[] outputColumns = new String[columnIndexes.length];
+			String[] lineTokens = line.split(delimiter);
+			int outputIndex = 0;
+			for (int columnIndex : columnIndexes) {
+				ensureColumnIndexIsValid(columnIndex, line, lineTokens);
+				outputColumns[outputIndex++] = lineTokens[columnIndex];
+			}
+			return outputColumns;
+		}
+	}
+
 	/**
 	 * Returns a List<String> containing the contents of the requested column in the input file,
 	 * assuming the file contains columns delimited by the delimiter String.
@@ -27,28 +94,20 @@ public class FileLoaderUtil {
 	 * @throws IOException
 	 */
 	public static List<String> loadColumnFromDelimitedFile(File inputFile, String encoding, String delimiter,
-			int columnIndex, String commentIndicator) throws IOException {
-		List<String> outputColumn = new ArrayList<String>();
-		for (Iterator<String> lineIter = getLineIterator(inputFile, encoding, commentIndicator); lineIter.hasNext();) {
-			String line = lineIter.next();
-			if (delimiter == null) {
-				outputColumn.add(line);
-			} else {
-				String[] lineTokens = line.split(delimiter);
-				if (isColumnIndexValid(columnIndex, line, lineTokens)) {
-					outputColumn.add(lineTokens[columnIndex]);
-				} else {
-					throw new ArrayIndexOutOfBoundsException(String.format(
-							"Column index %d does not exist on line: %s", columnIndex, line));
-				}
-			}
+			String commentIndicator, int columnIndex) throws IOException {
+		List<String[]> singleColumnAsArrayList = loadColumnsFromDelimitedFile(inputFile, encoding, delimiter,
+				commentIndicator, columnIndex);
+		List<String> outputList = new ArrayList<String>(singleColumnAsArrayList.size());
+		while (!singleColumnAsArrayList.isEmpty()) {
+			outputList.add(singleColumnAsArrayList.get(0)[0]);
+			singleColumnAsArrayList.remove(0);
 		}
-		return outputColumn;
+		return outputList;
 	}
 
-	public static List<String> loadColumnFromDelimitedFile(File inputFile, String delimiter, int columnIndex,
-			String commentIndicator) throws IOException {
-		return loadColumnFromDelimitedFile(inputFile, DEFAULT_ENCODING, delimiter, columnIndex, commentIndicator);
+	public static List<String> loadColumnFromDelimitedFile(File inputFile, String delimiter, String commentIndicator,
+			int columnIndex) throws IOException {
+		return loadColumnFromDelimitedFile(inputFile, DEFAULT_ENCODING, delimiter, commentIndicator, columnIndex);
 	}
 
 	/**
@@ -61,7 +120,7 @@ public class FileLoaderUtil {
 	 */
 	public static List<String> loadLinesFromFile(File inputFile, String encoding, String commentIndicator)
 			throws IOException {
-		return loadColumnFromDelimitedFile(inputFile, encoding, null, 0, commentIndicator);
+		return loadColumnFromDelimitedFile(inputFile, encoding, null, commentIndicator, 0);
 	}
 
 	/**
@@ -71,11 +130,24 @@ public class FileLoaderUtil {
 	 * @throws IOException
 	 */
 	public static List<String> loadLinesFromFile(File inputFile, String encoding) throws IOException {
-		return loadColumnFromDelimitedFile(inputFile, encoding, null, 0, null);
+		return loadColumnFromDelimitedFile(inputFile, encoding, null, null, 0);
 	}
 
 	public static List<String> loadLinesFromFile(File inputFile) throws IOException {
-		return loadColumnFromDelimitedFile(inputFile, DEFAULT_ENCODING, null, 0, null);
+		return loadColumnFromDelimitedFile(inputFile, DEFAULT_ENCODING, null, null, 0);
+	}
+
+	/**
+	 * Returns true if the input String[] contains a column for the input column Index, false
+	 * otherwise.
+	 * 
+	 * @param columnIndex
+	 * @param lineTokens
+	 * @return
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
+	private static boolean isColumnIndexValid(int columnIndex, String[] lineTokens) {
+		return (columnIndex > -1 && columnIndex < lineTokens.length);
 	}
 
 	/**
@@ -88,8 +160,13 @@ public class FileLoaderUtil {
 	 * @return
 	 * @throws ArrayIndexOutOfBoundsException
 	 */
-	private static boolean isColumnIndexValid(int columnIndex, String line, String[] lineTokens) {
-		return (columnIndex > -1 && columnIndex < lineTokens.length - 1);
+	private static void ensureColumnIndexIsValid(int columnIndex, String line, String[] lineTokens)
+			throws ArrayIndexOutOfBoundsException {
+		if (!isColumnIndexValid(columnIndex, lineTokens)) {
+			throw new ArrayIndexOutOfBoundsException(String.format(
+					"Column index %d does not exist on line. There are only %d columns on line: %s", columnIndex,
+					lineTokens.length, line));
+		}
 	}
 
 	/**
--- test/edu/ucdenver/ccp/util/file/FileLoaderUtilTest.java
+++ test/edu/ucdenver/ccp/util/file/FileLoaderUtilTest.java
@@ -1,10 +1,15 @@
 package edu.ucdenver.ccp.util.file;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -21,6 +26,8 @@ import edu.ucdenver.ccp.util.string.StringConstants;
 public class FileLoaderUtilTest {
 
 	private static final String COMMENT_INDICATOR = StringConstants.TWO_FORWARD_SLASHES;
+
+	private static final String IS_COLUMN_INDEX_VALID_METHOD_NAME = "isColumnIndexValid";
 
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
@@ -168,7 +175,7 @@ public class FileLoaderUtilTest {
 	public void testLoadFirstColumnFromSingleColumnFile() throws Exception {
 		List<String> expectedColumn = getOneColumnLines();
 		assertEquals(String.format("The single column in this file should be extracted"), expectedColumn,
-				FileLoaderUtil.loadColumnFromDelimitedFile(oneColumnFile, null, 0, null));
+				FileLoaderUtil.loadColumnFromDelimitedFile(oneColumnFile, null, null, 0));
 	}
 
 	@Test
@@ -185,44 +192,113 @@ public class FileLoaderUtilTest {
 	public void testLoadThirdColumnFromFiveColumnFile() throws Exception {
 		List<String> expectedColumn = CollectionsUtil.createList("13", "23", "33", "43", "53", "63");
 		assertEquals(String.format("The single column in this file should be extracted"), expectedColumn,
-				FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFile, RegExPatterns.TAB, 2, null));
+				FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFile, RegExPatterns.TAB, null, 2));
 	}
 
 	@Test
 	public void testLoadThirdColumnFromFiveColumnFileWithMissingDataOnLineThree() throws Exception {
 		List<String> expectedColumn = CollectionsUtil.createList("13", "23", "33", "43", "53", "63");
 		assertEquals(String.format("The single column in this file should be extracted"), expectedColumn,
-				FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithErrorOnLineThree, RegExPatterns.TAB, 2,
-						null));
+				FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithErrorOnLineThree, RegExPatterns.TAB, null,
+						2));
 	}
 
 	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void testLoadFifthColumnFromFiveColumnFileWithMissingDataOnLineThree() throws Exception {
-		FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithErrorOnLineThree, RegExPatterns.TAB, 5, null);
+		FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithErrorOnLineThree, RegExPatterns.TAB, null, 5);
 	}
 
 	@Test
 	public void testLoadThirdColumnFromFiveColumnFileWithCommentOnLineFour() throws Exception {
 		List<String> expectedColumn = CollectionsUtil.createList("13", "23", "33", "53", "63");
 		assertEquals(String.format("The single column in this file should be extracted"), expectedColumn,
-				FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithCommentOnLineFour, RegExPatterns.TAB, 2,
-						COMMENT_INDICATOR));
+				FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithCommentOnLineFour, RegExPatterns.TAB,
+						COMMENT_INDICATOR, 2));
 	}
 
 	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void testInvalidColumnIndex_too_high_ReturnsException() throws Exception {
-		FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithCommentOnLineFour, RegExPatterns.TAB, 15, null);
+		FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithCommentOnLineFour, RegExPatterns.TAB, null, 15);
 	}
 
 	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void testInvalidColumnIndex_too_low_ReturnsException() throws Exception {
-		FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithCommentOnLineFour, RegExPatterns.TAB, -1, null);
+		FileLoaderUtil.loadColumnFromDelimitedFile(fiveColumnFileWithCommentOnLineFour, RegExPatterns.TAB, null, -1);
 	}
 
 	@Test(expected = UnsupportedOperationException.class)
 	public void testRemoveThrowsException() throws Exception {
 		Iterator<String> lineIter = FileLoaderUtil.getLineIterator(oneColumnFile, null);
 		lineIter.remove();
+	}
+
+	@Test
+	public void testLoadColumnsTwoThreeAndFiveFromFiveColumnFile() throws Exception {
+		List<String[]> expectedColumns = CollectionsUtil.createList(new String[] { "12", "13", "15" }, new String[] {
+				"22", "23", "25" }, new String[] { "32", "33", "35" }, new String[] { "42", "43", "45" }, new String[] {
+				"52", "53", "55" }, new String[] { "62", "63", "65" });
+
+		List<String[]> extractedColumns = FileLoaderUtil.loadColumnsFromDelimitedFile(fiveColumnFile,
+				RegExPatterns.TAB, null, 1, 2, 4);
+		compareListsOfStringArrays(expectedColumns, extractedColumns);
+	}
+
+	@Test
+	public void testNullDelimiterReturnsEntireLine() throws Exception {
+		List<String[]> expectedColumns = CollectionsUtil.createList(new String[] { getFiveColumnLines().get(0) },
+				new String[] { getFiveColumnLines().get(1) }, new String[] { getFiveColumnLines().get(2) },
+				new String[] { getFiveColumnLines().get(3) }, new String[] { getFiveColumnLines().get(4) },
+				new String[] { getFiveColumnLines().get(5) });
+
+		List<String[]> extractedColumns = FileLoaderUtil.loadColumnsFromDelimitedFile(fiveColumnFile, null, null, 2, 3,
+				5);
+		compareListsOfStringArrays(expectedColumns, extractedColumns);
+	}
+
+	/**
+	 * Asserts that the input lists of String[] arrays are equal
+	 * 
+	 * @param expectedList
+	 * @param observedList
+	 */
+	private void compareListsOfStringArrays(List<String[]> expectedList, List<String[]> observedList) {
+		assertEquals(String.format("# of lines in observed list should be as expected."), expectedList.size(),
+				observedList.size());
+		for (int i = 0; i < expectedList.size(); i++) {
+			assertArrayEquals(String.format("Line %d should be as expected.", i), expectedList.get(i), observedList
+					.get(i));
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testIllegalArgumentExceptionIfDelimiterButNoColumnIndexes() throws Exception {
+		FileLoaderUtil.loadColumnsFromDelimitedFile(fiveColumnFile, RegExPatterns.TAB, null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testIllegalArgumentExceptionIfDelimiterButEmptyColumnIndexes() throws Exception {
+		FileLoaderUtil.loadColumnsFromDelimitedFile(fiveColumnFile, RegExPatterns.TAB, null, new int[0]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testIllegalArgumentExceptionIfDelimiterButNullColumnIndexes() throws Exception {
+		FileLoaderUtil.loadColumnsFromDelimitedFile(fiveColumnFile, RegExPatterns.TAB, null, null);
+	}
+
+	@Test
+	public void testIsColumnIndexValid() throws Exception {
+		assertTrue(callIsColumnIndexValid(0, new String[3]));
+		assertTrue(callIsColumnIndexValid(1, new String[3]));
+		assertTrue(callIsColumnIndexValid(2, new String[3]));
+		assertFalse(callIsColumnIndexValid(3, new String[3]));
+	}
+
+	private boolean callIsColumnIndexValid(int columnIndex, String[] columns) throws SecurityException,
+			NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		Method method = FileLoaderUtil.class.getDeclaredMethod(IS_COLUMN_INDEX_VALID_METHOD_NAME, int.class,
+				String[].class);
+		method.setAccessible(true);
+		return (Boolean) method.invoke(null, columnIndex, columns);
 	}
 
 }
