260db7798eb71096d7d49e2e3ca55293dfb18171
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-04-06 18:40:08 +0000

    added javadoc

--- src/edu/ucdenver/ccp/parser/ncbi/pmc/PmcIdsCsvFileParser.java
+++ src/edu/ucdenver/ccp/parser/ncbi/pmc/PmcIdsCsvFileParser.java
@@ -7,6 +7,31 @@ import edu.ucdenver.ccp.parser.LineFileParser;
 import edu.ucdenver.ccp.util.file.FileLoaderUtil;
 import edu.ucdenver.ccp.util.string.StringConstants;
 
+/**
+ * This class implements a parser for the PubMed Central PMC-ids.csv file available at the PMC FTP
+ * site: ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/PMC-ids.csv.gz<br>
+ * <br>
+ * Format: <br>
+ * 
+ * <pre>
+ * Journal Title,ISSN,eISSN,Year,Volume,Issue,Page,DOI,PMC accession number,PMID,Manuscript Id,Release Date
+ * </pre>
+ * 
+ * Sample entries:<br>
+ * 
+ * <pre>
+ * Mol Biol Cell,1059-1524,1939-4586,2000,11,6,2019, ,PMC14900,10848626, ,live
+ * J Neurosci,0270-6474,1529-2401,2005,25,24,5740,10.1523/JNEUROSCI.0913-05.2005,PMC1201448,15958740,NIHMS3372,live
+ * Cancer Res,0008-5472,1538-7445,2007,67,17,8022,10.1158/0008-5472.CAN-06-3749,PMC1986634,17804713,NIHMS25090,Sep 1 2008
+ * Proc Natl Acad Sci U S A,0027-8424,1091-6490,2007,104,43,17075,10.1073/pnas.0707060104,PMC2040460,17940018, ,live
+ * Cell Host Microbe,1931-3128,1934-6069,2007,2,6,404,10.1016/j.chom.2007.09.014,PMC2184509,18078692,NIHMS36164,live
+ * Proc Natl Acad Sci U S A,0027-8424,1091-6490,2008,105,21,7382,10.1073/pnas.0711174105,PMC2396716,18495922, ,Nov 27 2008
+ * PLoS Med,1549-1277,1549-1676,2008, ,Immediate Access,e168,10.1371/journal.pmed.0050168,PMC2494565,18684010, ,live
+ * </pre>
+ * 
+ * @author Bill Baumgartner
+ * 
+ */
 public class PmcIdsCsvFileParser extends LineFileParser<PmcIdsCsvFileData> {
 	public static final String DELIMITER = StringConstants.COMMA;
 	private static final int FILE_COLUMN_COUNT = 12;
