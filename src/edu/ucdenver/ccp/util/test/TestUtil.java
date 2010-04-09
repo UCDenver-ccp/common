771878b344bf09d3d14e11dca997b5c3e37b7dbf
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-04-09 21:52:00 +0000

    Re-added conductBeanComparison() method and supporting methods.

--- src/edu/ucdenver/ccp/util/test/TestUtil.java
+++ src/edu/ucdenver/ccp/util/test/TestUtil.java
@@ -28,7 +28,7 @@ import edu.ucdenver.ccp.util.string.RegExPatterns;
  * 
  */
 public class TestUtil {
-	
+
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(TestUtil.class);
 
@@ -53,6 +53,64 @@ public class TestUtil {
 		return file;
 	}
 
-	
+	public static void conductBeanComparison(Object expected, Object observed) throws ArrayComparisonFailure,
+			IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		assertEquals(String.format("Beans must be the same class in order to be compared."), expected.getClass()
+				.getName(), observed.getClass().getName());
+		Collection<Method> getterMethods = getGetterMethods(expected);
+		for (Method method : getterMethods) {
+			String fieldName = method.getName().substring(3);
+			if (method.getReturnType().getName().startsWith("[I")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (int[]) method
+						.invoke(expected), (int[]) method.invoke(observed));
+			} else if (method.getReturnType().getName().startsWith("[B")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (byte[]) method
+						.invoke(expected), (byte[]) method.invoke(observed));
+			} else if (method.getReturnType().getName().startsWith("[C")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (char[]) method
+						.invoke(expected), (char[]) method.invoke(observed));
+			} else if (method.getReturnType().getName().startsWith("[F")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (float[]) method
+						.invoke(expected), (float[]) method.invoke(observed), 0.000001f);
+			} else if (method.getReturnType().getName().startsWith("[D")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (double[]) method
+						.invoke(expected), (double[]) method.invoke(observed), 0.000001d);
+			} else if (method.getReturnType().getName().startsWith("[J")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (long[]) method
+						.invoke(expected), (long[]) method.invoke(observed));
+			} else if (method.getReturnType().getName().startsWith("[S")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (short[]) method
+						.invoke(expected), (short[]) method.invoke(observed));
+			} else if (method.getReturnType().getName().startsWith("[")) {
+				assertArrayEquals(String.format("Value for field %s must be the same.", fieldName), (Object[]) method
+						.invoke(expected), (Object[]) method.invoke(observed));
+			} else {
+				assertEquals(String.format("Value for field %s must be the same.", fieldName), method.invoke(expected),
+						method.invoke(observed));
+			}
+		}
+	}
+
+	private static Collection<Method> getGetterMethods(Object obj) {
+		Collection<Method> getterMethodNames = new ArrayList<Method>();
+		for (Method method : obj.getClass().getMethods()) {
+			if (isGetterMethod(method)) {
+				getterMethodNames.add(method);
+			}
+		}
+		return getterMethodNames;
+	}
+
+	/**
+	 * Returns true if the method starts with get followed by an upper case letter, false otherwise
+	 * 
+	 * @param method
+	 * @return
+	 */
+	private static boolean isGetterMethod(Method method) {
+		Pattern getMethodPattern = Pattern.compile(RegExPatterns.GETTER_METHOD_NAME_PATTERN);
+		Matcher m = getMethodPattern.matcher(method.getName());
+		return m.find();
+	}
 
 }
