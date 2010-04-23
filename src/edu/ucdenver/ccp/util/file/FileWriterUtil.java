2fb985cf0d78260a5441b2822d5dbf823d800815
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-04-23 19:42:51 +0000

    Added a printLines(lines, File) method

--- src/edu/ucdenver/ccp/util/file/FileWriterUtil.java
+++ src/edu/ucdenver/ccp/util/file/FileWriterUtil.java
@@ -1,5 +1,7 @@
 package edu.ucdenver.ccp.util.file;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.List;
 
@@ -15,6 +17,20 @@ public class FileWriterUtil {
 		for (String line : lines) {
 			ps.println(line);
 		}
+	}
+
+	/**
+	 * Prints the input list of lines to the specified file. The file is overwritten with the input
+	 * lines.
+	 * 
+	 * @param lines
+	 * @param file
+	 * @throws FileNotFoundException
+	 */
+	public static void printLines(List<String> lines, File file) throws FileNotFoundException {
+		PrintStream ps = new PrintStream(file);
+		printLines(lines, ps);
+		ps.close();
 	}
 
 }
--- test/edu/ucdenver/ccp/util/file/FileWriterUtilTest.java
+++ test/edu/ucdenver/ccp/util/file/FileWriterUtilTest.java
@@ -13,7 +13,6 @@ import org.junit.rules.TemporaryFolder;
 
 import edu.ucdenver.ccp.util.collections.CollectionsUtil;
 
-
 public class FileWriterUtilTest {
 
 	@Rule
@@ -21,20 +20,30 @@ public class FileWriterUtilTest {
 
 	private PrintStream ps;
 	private File outputFile;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		outputFile = folder.newFile("outputFile.txt");
 		ps = new PrintStream(outputFile);
 	}
-	
+
 	@Test
 	public void testPrintLines() throws Exception {
-		List<String> lines = CollectionsUtil.createList("line 1","line 2","line 3", "line 4");
+		List<String> lines = CollectionsUtil.createList("line 1", "line 2", "line 3", "line 4");
 		FileWriterUtil.printLines(lines, ps);
 		ps.close();
 		List<String> linesWritten = FileLoaderUtil.loadLinesFromFile(outputFile);
-		assertEquals(String.format("Lines read from the output file should equal the lines written to it."), linesWritten, lines);
+		assertEquals(String.format("Lines read from the output file should equal the lines written to it."),
+				linesWritten, lines);
 	}
-	
+
+	@Test
+	public void testPrintLinesToFile() throws Exception {
+		File testFile = folder.newFile("test.txt");
+		List<String> expectedLines = CollectionsUtil.createList("line1", "line2", "line3");
+		FileWriterUtil.printLines(expectedLines, testFile);
+		List<String> lines = FileLoaderUtil.loadLinesFromFile(testFile);
+		assertEquals(String.format("Should have the 3 expected lines in the file."), expectedLines, lines);
+	}
+
 }
