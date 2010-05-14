9ad140ed2712df3e1ebda47f2bfa88ded92f1ed1
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-05-14 16:39:55 +0000

    Added a logger and a method to copy a file from the classpath to a temporary location

--- src/main/java/edu/ucdenver/ccp/util/test/DefaultTestCase.java
+++ src/main/java/edu/ucdenver/ccp/util/test/DefaultTestCase.java
@@ -1,5 +1,7 @@
 package edu.ucdenver.ccp.util.test;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.log4j.BasicConfigurator;
@@ -9,6 +11,8 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
+import edu.ucdenver.ccp.util.file.FileUtil;
+
 /**
  * A super class for other test classes. It sets up a logger among other things.
  * 
@@ -16,6 +20,7 @@ import org.junit.rules.TemporaryFolder;
  * 
  */
 public class DefaultTestCase {
+	private static final Logger logger = Logger.getLogger(DefaultTestCase.class);
 
 	static {
 		BasicConfigurator.configure();
@@ -33,8 +38,25 @@ public class DefaultTestCase {
 	 * @return
 	 */
 	protected InputStream getResourceFromClasspath(Class<?> clazz, String resourceName) {
-		InputStream is =  clazz.getResourceAsStream(resourceName);
+		InputStream is = clazz.getResourceAsStream(resourceName);
 		Assert.assertNotNull(is);
 		return is;
+	}
+
+	/**
+	 *Helper method for grabbing a file from the classpath and copying it to the temporary folder.
+	 * This method can be used for testing applications that require File input, but are incapable
+	 * of consuming InputStreams.
+	 * 
+	 * @param clazz
+	 * @param resourceName
+	 * @return
+	 * @throws IOException
+	 */
+	protected File copyClasspathResourceToTemporaryFile(Class<?> clazz, String resourceName) throws IOException {
+		File tempFile = folder.newFile(resourceName);
+		FileUtil.copy(getResourceFromClasspath(clazz, resourceName), tempFile);
+		logger.debug(String.format("created file: %s", tempFile.getAbsolutePath()));
+		return tempFile;
 	}
 }
