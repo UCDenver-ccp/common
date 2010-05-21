89731331f7b1a8a2ed801dc58937539ecde83457
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-05-21 19:13:16 +0000

    Initial commit of the @FtpDownload annotation that implements download of a single file via FTP for a class member variable of type java.io.File.

---
+++ src/main/java/edu/ucdenver/ccp/util/download/FtpDownload.java
@@ -0,0 +1,22 @@
+package edu.ucdenver.ccp.util.download;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import edu.ucdenver.ccp.util.ftp.FTPUtil.FileType;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface FtpDownload {
+    String server();
+    int port() default -1;
+    String path();
+    String filename();
+    FileType filetype();
+    String username() default "anonymous";
+    String password() default "anonymous";
+}
