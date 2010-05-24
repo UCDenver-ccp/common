65ce49e2580e8e158791016ab2d05d02082cd118
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-05-24 15:51:16 +0000

    Modified invokePrivateMethod() so that it looks for a specific method (with specific input parameters). Previously this method cycled through all methods and just looked for the correct method name. This had the potential to fail unexpectedly if there was an overloaded method in a class (same name, different parameters). The parameter types are now inferred by the input parameters.

--- src/main/java/edu/ucdenver/ccp/util/test/PrivateAccessor.java
+++ src/main/java/edu/ucdenver/ccp/util/test/PrivateAccessor.java
@@ -7,58 +7,46 @@ import java.lang.reflect.Method;
 import junit.framework.Assert;
 
 /**
- * Provides access to private members in classes. Modified from http://snippets.dzone.com/posts/show/2242
+ * Provides access to private members in classes. Modified from
+ * http://snippets.dzone.com/posts/show/2242
  */
 public class PrivateAccessor {
-	
-  public static Object getPrivateField (Object o, String fieldName) {   
-	 // Check we have valid arguments... 
-    Assert.assertNotNull(o);
-    Assert.assertNotNull(fieldName);
-    
-    // Go and find the private field... 
-    final Field fields[] = o.getClass().getDeclaredFields();
-    for (int i = 0; i < fields.length; ++i) {
-      if (fieldName.equals(fields[i].getName())) {
-        try {
-          fields[i].setAccessible(true);
-          return fields[i].get(o);
-        } 
-        catch (IllegalAccessException ex) {
-          Assert.fail ("IllegalAccessException accessing " + fieldName);
-        }
-      }
-    }
-    Assert.fail ("Field '" + fieldName +"' not found");
-    return null;
-  }
-  
-  public static Object invokePrivateMethod (Object o, String methodName, Object... params) {   
-		 // Check we have valid arguments... 
-	    Assert.assertNotNull(o);
-	    Assert.assertNotNull(methodName);
-	    Assert.assertNotNull(params);
-	    
-	    // Go and find the private method... 
-	    final Method methods[] = o.getClass().getDeclaredMethods();
-	    for (int i = 0; i < methods.length; ++i) {
-	      if (methodName.equals(methods[i].getName())) {
-	        try {
-	          methods[i].setAccessible(true);
-	          return methods[i].invoke(o, params);
-	        } 
-	        catch (IllegalAccessException ex) {
-	          Assert.fail ("IllegalAccessException accessing " + methodName);
-	        }
-	        catch (InvocationTargetException ite) {
-	        	ite.printStackTrace();
-	        	Assert.fail ("InvocationTargetException accessing " + methodName);	        	
-	        }
-	      }
-	    }
-	    Assert.fail ("Method '" + methodName +"' not found");
-	    return null;
-	  }  
-  
-}
 
+	public static Object getPrivateField(Object o, String fieldName) {
+		// Check we have valid arguments...
+		Assert.assertNotNull(o);
+		Assert.assertNotNull(fieldName);
+
+		// Go and find the private field...
+		final Field fields[] = o.getClass().getDeclaredFields();
+		for (int i = 0; i < fields.length; ++i) {
+			if (fieldName.equals(fields[i].getName())) {
+				try {
+					fields[i].setAccessible(true);
+					return fields[i].get(o);
+				} catch (IllegalAccessException ex) {
+					Assert.fail("IllegalAccessException accessing " + fieldName);
+				}
+			}
+		}
+		Assert.fail("Field '" + fieldName + "' not found");
+		return null;
+	}
+
+	public static Object invokePrivateMethod(Object o, String methodName, Object... params) throws SecurityException,
+			NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		Assert.assertNotNull(o);
+		Assert.assertNotNull(methodName);
+		Assert.assertNotNull(params);
+
+		Class<?>[] paramClses = new Class<?>[params.length];
+		for (int i = 0; i < params.length; i++) {
+			paramClses[i] = params[i].getClass();
+		}
+
+		final Method method = o.getClass().getDeclaredMethod(methodName, paramClses);
+		method.setAccessible(true);
+		return method.invoke(o, params);
+	}
+
+}
