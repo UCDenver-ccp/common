40f64caa193882ac273e369db17ec5e8bb89693c
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-06-09 15:03:37 +0000

    Removed unnecessary imports to get rid of compiler warning.

--- src/main/java/edu/ucdenver/ccp/util/string/StringUtil.java
+++ src/main/java/edu/ucdenver/ccp/util/string/StringUtil.java
@@ -3,12 +3,9 @@ package edu.ucdenver.ccp.util.string;
 import static edu.ucdenver.ccp.util.string.RegExPatterns.HAS_NUMBERS_ONLY_OPT_NEG;
 import static edu.ucdenver.ccp.util.string.RegExPatterns.HAS_NUMBERS_ONLY_OPT_NEG_ZERO_START;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
