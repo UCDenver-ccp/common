b71bd52abf728b2476bf79ea980792bcb9e20fab
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-10-21 19:03:24 +0000

    Organized imports to remove a compiler warning.

--- src/main/java/edu/ucdenver/ccp/common/collections/CollectionsUtil.java
+++ src/main/java/edu/ucdenver/ccp/common/collections/CollectionsUtil.java
@@ -21,7 +21,6 @@ package edu.ucdenver.ccp.common.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
