84ea9e8181853554a10743366850e223d53f3b7f
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2010-10-21 19:02:54 +0000

    Added the checkIterator() method for dealing with a raw-type Iterator class and converting it to a generified Iterator.

--- src/main/java/edu/ucdenver/ccp/common/collections/LegacyCollectionsUtil.java
+++ src/main/java/edu/ucdenver/ccp/common/collections/LegacyCollectionsUtil.java
@@ -2,6 +2,7 @@ package edu.ucdenver.ccp.common.collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -25,7 +26,7 @@ public class LegacyCollectionsUtil {
 	 */
 	/*
 	 * This method deals with the raw-type List. Because of this, type checking cannot be done by
-	 * the compiler and is instead done at runtime therefore it is safe to ignore the "unchecked"
+	 * the compiler and is instead done at runtime, therefore it is safe to suppress the "unchecked"
 	 * compiler warnings.
 	 */
 	@SuppressWarnings("unchecked")
@@ -34,4 +35,42 @@ public class LegacyCollectionsUtil {
 		checkedList.addAll(list);
 		return checkedList;
 	}
+
+	/**
+	 * This utility method ensures the content of the input raw-type Iterator is as expected in
+	 * regards to the Classes it returns. Type checking is done at runtime in this case so compiler
+	 * warnings for unchecked types can be suppressed.
+	 * 
+	 * @param <T>
+	 * @param iter
+	 * @param clazz
+	 * @return
+	 */
+	/*
+	 * This method deals with the raw-type Iterator. Because of this, type checking cannot be done
+	 * by the compiler and is instead done at runtime, therefore it is safe to suppress the
+	 * "unchecked" compiler warnings.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> Iterator<T> checkIterator(final Iterator iter, final Class<T> clazz) {
+		return new Iterator<T>() {
+
+			@Override
+			public boolean hasNext() {
+				return iter.hasNext();
+			}
+
+			@Override
+			public T next() {
+				return clazz.cast(iter.next());
+			}
+
+			@Override
+			public void remove() {
+				iter.remove();
+			}
+
+		};
+	}
+
 }
--- src/test/java/edu/ucdenver/ccp/common/collections/LegacyCollectionsUtilTest.java
+++ src/test/java/edu/ucdenver/ccp/common/collections/LegacyCollectionsUtilTest.java
@@ -2,10 +2,13 @@ package edu.ucdenver.ccp.common.collections;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
+
+import edu.ucdenver.ccp.common.test.TestUtil;
 
 public class LegacyCollectionsUtilTest {
 
@@ -18,9 +21,7 @@ public class LegacyCollectionsUtilTest {
 	public void testCheckList_HomogeneousListInput() throws Exception {
 		List<String> expectedList = CollectionsUtil.createList("string 1", "string 2");
 
-		List legacyList = new ArrayList();
-		legacyList.add("string 1");
-		legacyList.add("string 2");
+		List legacyList = initRawTypeList("string 1", "string 2");
 
 		List<String> list = LegacyCollectionsUtil.checkList(legacyList, String.class);
 		assertEquals(String.format("Generic list should convert without error and match the expected."), expectedList,
@@ -34,11 +35,61 @@ public class LegacyCollectionsUtilTest {
 	@SuppressWarnings("unchecked")
 	@Test(expected = ClassCastException.class)
 	public void testCheckList_HeterogeneousListInput() throws Exception {
-		List legacyList = new ArrayList();
-		legacyList.add("string 1");
-		legacyList.add(new Integer(1));
-
+		List legacyList = initRawTypeList("string 1", new Integer(1));
 		LegacyCollectionsUtil.checkList(legacyList, String.class);
+	}
+
+	/*
+	 * The warnings being suppressed in this method are due to the creation of a legacy raw-type
+	 * Iterator, which is necessary in order to test the LegacyCollectionsUtil.checkIterator() method.
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testCheckIterator_HomogeneousInput() throws Exception {
+		String value1 = "1";
+		String value2 = "2";
+		String value3 = "3";
+		
+		Iterator legacyIter = initRawTypeList(value1, value2, value3).iterator();
+		
+		Iterator<String> generifiedIter = LegacyCollectionsUtil.checkIterator(legacyIter, String.class);
+		TestUtil.checkNext(generifiedIter, value1);
+		TestUtil.checkNext(generifiedIter, value2);
+		TestUtil.checkNext(generifiedIter, value3);
+	}
+
+	/*
+	 * The warnings being suppressed in this method are due to the creation of a legacy raw-type
+	 * Iterator, which is necessary in order to test the LegacyCollectionsUtil.checkIterator() method.
+	 */
+	@SuppressWarnings("unchecked")
+	@Test(expected = ClassCastException.class)
+	public void testCheckIterator_HeterogeneousInput() throws Exception {
+		String value1 = "1";
+		String value2 = "2";
+		Integer value3 = new Integer(3);
+		
+		Iterator legacyIter = initRawTypeList(value1, value2, value3).iterator();
+		
+		Iterator<String> generifiedIter = LegacyCollectionsUtil.checkIterator(legacyIter, String.class);
+		TestUtil.checkNext(generifiedIter, value1);
+		TestUtil.checkNext(generifiedIter, value2);
+		generifiedIter.next();
+	}
+	
+	/**
+	 * Initialize a raw-type list using the input list members
+	 * 
+	 * @param listMembers
+	 * @return
+	 */
+	/*
+	 * The warnings are being suppressed in this method because we are intentionally creating a
+	 * raw-type List
+	 */
+	@SuppressWarnings("unchecked")
+	private List initRawTypeList(Object... listMembers) {
+		return Arrays.asList(listMembers);
 	}
 
 }
