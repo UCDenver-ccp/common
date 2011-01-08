c5d4bd79ca0135c34eb8ef3749c520d1d8f68114
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2011-01-08 14:01:39 +0000

    Added @SuppressWarnings("rawtypes") annotations to raw-type collections that are intentionally created in LegacyCollectionsUtilTest in order to exercise the methods in LegacyCollectionsUtil that expect raw-type collection objects.

--- src/test/java/edu/ucdenver/ccp/common/collections/LegacyCollectionsUtilTest.java
+++ src/test/java/edu/ucdenver/ccp/common/collections/LegacyCollectionsUtilTest.java
@@ -20,6 +20,7 @@ public class LegacyCollectionsUtilTest {
 	public void testCheckList_HomogeneousListInput() {
 		List<String> expectedList = CollectionsUtil.createList("string 1", "string 2");
 
+		@SuppressWarnings("rawtypes")
 		List legacyList = initRawTypeList("string 1", "string 2");
 
 		List<String> list = LegacyCollectionsUtil.checkList(legacyList, String.class);
@@ -33,22 +34,25 @@ public class LegacyCollectionsUtilTest {
 	 */
 	@Test(expected = ClassCastException.class)
 	public void testCheckList_HeterogeneousListInput() {
+		@SuppressWarnings("rawtypes")
 		List legacyList = initRawTypeList("string 1", new Integer(1));
 		LegacyCollectionsUtil.checkList(legacyList, String.class);
 	}
 
 	/*
 	 * The warnings being suppressed in this method are due to the creation of a legacy raw-type
-	 * Iterator, which is necessary in order to test the LegacyCollectionsUtil.checkIterator() method.
+	 * Iterator, which is necessary in order to test the LegacyCollectionsUtil.checkIterator()
+	 * method.
 	 */
 	@Test
 	public void testCheckIterator_HomogeneousInput() {
 		String value1 = "1";
 		String value2 = "2";
 		String value3 = "3";
-		
+
+		@SuppressWarnings("rawtypes")
 		Iterator legacyIter = initRawTypeList(value1, value2, value3).iterator();
-		
+
 		Iterator<String> generifiedIter = LegacyCollectionsUtil.checkIterator(legacyIter, String.class);
 		TestUtil.checkNext(generifiedIter, value1);
 		TestUtil.checkNext(generifiedIter, value2);
@@ -57,29 +61,35 @@ public class LegacyCollectionsUtilTest {
 
 	/*
 	 * The warnings being suppressed in this method are due to the creation of a legacy raw-type
-	 * Iterator, which is necessary in order to test the LegacyCollectionsUtil.checkIterator() method.
+	 * Iterator, which is necessary in order to test the LegacyCollectionsUtil.checkIterator()
+	 * method.
 	 */
 	@Test(expected = ClassCastException.class)
 	public void testCheckIterator_HeterogeneousInput() {
 		String value1 = "1";
 		String value2 = "2";
 		Integer value3 = new Integer(3);
-		
+
+		@SuppressWarnings("rawtypes")
 		Iterator legacyIter = initRawTypeList(value1, value2, value3).iterator();
-		
+
 		Iterator<String> generifiedIter = LegacyCollectionsUtil.checkIterator(legacyIter, String.class);
 		TestUtil.checkNext(generifiedIter, value1);
 		TestUtil.checkNext(generifiedIter, value2);
 		generifiedIter.next();
 	}
-	
+
 	/**
 	 * Initialize a raw-type list using the input list members
 	 * 
 	 * @param listMembers
 	 * @return
 	 */
-	private List initRawTypeList(Object... listMembers) {
+	/*
+	 * The warnings being suppressed in this method are due to the intentional creation of a legacy raw-type
+	 * List
+	 */
+	private @SuppressWarnings("rawtypes") List initRawTypeList(Object... listMembers) {
 		return Arrays.asList(listMembers);
 	}
 
