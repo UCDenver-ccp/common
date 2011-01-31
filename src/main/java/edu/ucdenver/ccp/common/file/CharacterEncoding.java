0f487a8a80f6848d956b0fbd53622de2a986ea41
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2011-01-31 17:41:42 +0000

    Resolving Eclipse warnings. Added JavaDoc. Addressed some FindBugs bugs.

--- src/main/java/edu/ucdenver/ccp/common/file/CharacterEncoding.java
+++ src/main/java/edu/ucdenver/ccp/common/file/CharacterEncoding.java
@@ -1,7 +1,10 @@
 package edu.ucdenver.ccp.common.file;
 
 import java.io.File;
-import java.nio.charset.*;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CodingErrorAction;
 
 /**
  * This enum ties together various character encodings with canonical file suffixes used to indicate
@@ -12,30 +15,72 @@ import java.nio.charset.*;
  */
 public enum CharacterEncoding {
 
-	US_ASCII("US-ASCII", ".ascii"), UTF_8("UTF-8", ".utf8"), ISO_8859_1("ISO-8859-1", ".iso8859-1");
+	/**
+	 * Signifies the ASCII character set
+	 */
+	US_ASCII("US-ASCII", ".ascii"),
+	/**
+	 * Signifies UTF-8 character set
+	 */
+	UTF_8("UTF-8", ".utf8"),
+	/**
+	 * Signifies ISO-8859-1 character set
+	 */
+	ISO_8859_1("ISO-8859-1", ".iso8859-1");
 
+	/**
+	 * The official name of the character set
+	 */
 	private final String characterSetName;
+
+	/**
+	 * A standard file suffix to use with this particular character set
+	 */
 	private final String fileSuffix;
 
+	/**
+	 * Private constructor for initializing CharacterEncoding enum elements
+	 * 
+	 * @param characterSetName
+	 * @param fileSuffix
+	 */
 	private CharacterEncoding(String characterSetName, String fileSuffix) {
 		this.characterSetName = characterSetName;
 		this.fileSuffix = fileSuffix;
 	}
 
+	/**
+	 * 
+	 * @return the name of the character set
+	 */
 	public String getCharacterSetName() {
 		return characterSetName;
 	}
 
+	/**
+	 * @return the file suffix associated with this particular character set
+	 */
 	public String getFileSuffix() {
 		return fileSuffix;
 	}
 
+	/**
+	 * @return a <code>CharsetEncoder</code> used to encode characters. Fails loudly if an encoding
+	 *         discrepancy is observed.
+	 */
 	public CharsetEncoder getEncoder() {
-		return Charset.forName(getCharacterSetName()).newEncoder().onMalformedInput(CodingErrorAction.REPORT).onUnmappableCharacter(CodingErrorAction.REPORT);
+		return Charset.forName(getCharacterSetName()).newEncoder().onMalformedInput(CodingErrorAction.REPORT)
+				.onUnmappableCharacter(CodingErrorAction.REPORT);
 	}
 
+	/**
+	 * 
+	 * @return a <code>CharsetDecoder</code> used to decode characters. Fails loudly if an encoding
+	 *         discrepancy is observed.
+	 */
 	public CharsetDecoder getDecoder() {
-		return Charset.forName(getCharacterSetName()).newDecoder().onMalformedInput(CodingErrorAction.REPORT).onUnmappableCharacter(CodingErrorAction.REPORT);
+		return Charset.forName(getCharacterSetName()).newDecoder().onMalformedInput(CodingErrorAction.REPORT)
+				.onUnmappableCharacter(CodingErrorAction.REPORT);
 	}
 
 	/**
@@ -47,10 +92,11 @@ public enum CharacterEncoding {
 	 * @return
 	 */
 	public static String getEncodingSpecificFileName(String fileName, CharacterEncoding encoding) {
+		String encodingSpecificFileName = fileName;
 		String fileSuffix = encoding.getFileSuffix();
 		if (!hasEncodingSpecificFileName(fileName, encoding))
-			fileName = fileName + fileSuffix;
-		return fileName;
+			encodingSpecificFileName = fileName + fileSuffix;
+		return encodingSpecificFileName;
 	}
 
 	/**
@@ -62,10 +108,11 @@ public enum CharacterEncoding {
 	 * @return
 	 */
 	public static File getEncodingSpecificFile(File file, CharacterEncoding encoding) {
+		File encodingSpecificFile = file;
 		String fileSuffix = encoding.getFileSuffix();
 		if (!hasEncodingSpecificFileName(file, encoding))
-			file = new File(file.getAbsolutePath() + fileSuffix);
-		return file;
+			encodingSpecificFile = new File(file.getAbsolutePath() + fileSuffix);
+		return encodingSpecificFile;
 	}
 
 	/**
