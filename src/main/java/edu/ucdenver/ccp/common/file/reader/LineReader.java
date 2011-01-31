f85a29d520272d50e2b76d66375ca1e6db608245
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2011-01-31 17:54:48 +0000

    Resolving Eclipse warnings. Added JavaDoc.

--- src/main/java/edu/ucdenver/ccp/common/file/reader/LineReader.java
+++ src/main/java/edu/ucdenver/ccp/common/file/reader/LineReader.java
@@ -21,58 +21,103 @@ package edu.ucdenver.ccp.common.file.reader;
 import java.io.Closeable;
 import java.io.IOException;
 
+/**
+ * Abstract class for reading a collection of <code>Line</code> objects.
+ * 
+ * @author bill
+ * 
+ */
 public abstract class LineReader implements Closeable {
 
+	/**
+	 * The skipLinePrefix member variable is used as an indication that a line should be skipped
+	 * (and not "read" by the LineReader). If a line starts with the skip line prefix then it is
+	 * skipped
+	 */
 	protected final String skipLinePrefix;
 
+	/**
+	 * Constructor for the abstract LineReader
+	 * 
+	 * @param skipLinePrefix
+	 *            if a line starts with the skip line prefix, then it is skipped (and not returned
+	 *            by the LineReader)
+	 */
 	public LineReader(String skipLinePrefix) {
 		this.skipLinePrefix = skipLinePrefix;
 	}
 
+	/**
+	 * Abstract method that defines how an implementation of LineReader reads a line
+	 * 
+	 * @return the most recently read Line
+	 * @throws IOException
+	 */
 	public abstract Line readLine() throws IOException;
 
+	/**
+	 * This method checks to see if a line should be skipped. It returns true if the line starts
+	 * with the skip line prefix, fale otherwise.
+	 * 
+	 * @param line
+	 * @return
+	 */
 	protected boolean skipLine(String line) {
 		if (skipLinePrefix == null)
 			return false;
 		return line.trim().startsWith(skipLinePrefix);
 	}
 
+	/**
+	 * Simple class for defining a line
+	 * 
+	 * @author bill
+	 * 
+	 */
 	public static class Line {
+		/**
+		 * The text contained on this Line
+		 */
 		protected final String text;
+		/**
+		 * Stores the line number (relative to the specific collection from where the line was read)
+		 */
 		protected final int lineNumber;
 
+		/**
+		 * Initializes a new <code>Line</code>
+		 * 
+		 * @param text
+		 * @param lineNumber
+		 */
 		public Line(String text, int lineNumber) {
 			super();
 			this.text = text;
 			this.lineNumber = lineNumber;
 		}
 
+		/**
+		 * @return the text of this particular line
+		 */
 		public String getText() {
 			return text;
 		}
 
+		/**
+		 * @return the line number for this particular line
+		 */
 		public int getLineNumber() {
 			return lineNumber;
 		}
 
+		/**
+		 * Returns a string representation of this line including the line number
+		 */
 		@Override
 		public String toString() {
 			return String.format("(Line:%d) %s", lineNumber, text);
 		}
 
-	}
-
-	public static class FileLine extends Line {
-		private final long byteOffset;
-
-		public FileLine(String text, int lineNumber, long byteOffset) {
-			super(text, lineNumber);
-			this.byteOffset = byteOffset;
-		}
-
-		public long getByteOffset() {
-			return byteOffset;
-		}
 	}
 
 }
