d0f68b13545cc7b0b336be79c026fc4ba17eae73
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2011-03-25 23:20:03 +0000

    CDIST-33: Added a method to escape a character, and a method to return an XML header line. Cleaned up the other method by introducing constants and replacing the Strings that were being used.

--- src/main/java/edu/ucdenver/ccp/common/xml/XmlUtil.java
+++ src/main/java/edu/ucdenver/ccp/common/xml/XmlUtil.java
@@ -18,6 +18,9 @@
 
 package edu.ucdenver.ccp.common.xml;
 
+import edu.ucdenver.ccp.common.file.CharacterEncoding;
+import edu.ucdenver.ccp.common.string.StringConstants;
+
 /**
  * Utility class for dealing with XML content
  * 
@@ -27,13 +30,85 @@ package edu.ucdenver.ccp.common.xml;
 public class XmlUtil {
 
 	/**
-	 * Returns the input String with XML special characters converted to their XML-safe formats.
+	 * XML code for >
+	 */
+	public static final String GREATER_THAN_SIGN_CODE = "&gt;";
+
+	/**
+	 * XML code for <
+	 */
+	public static final String LESS_THAN_SIGN_CODE = "&lt;";
+
+	/**
+	 * XML code for '
+	 */
+	public static final String APOSTROPHE_CODE = "&apos;";
+
+	/**
+	 * XML code for "
+	 */
+	public static final String QUOTATION_MARK_CODE = "&quot;";
+
+	/**
+	 * XML code for &
+	 */
+	public static final String AMPERSAND_CODE = "&amp;";
+
+	/**
+	 * Returns the input String with XML special characters converted to their XML-safe formats, e.g
+	 * & --> &amp;
 	 * 
 	 * @param inputStr
-	 * @return
+	 *            the {@link String} to be processed
+	 * @return the input string with all XML special characters replaced by their corresponding XML
+	 *         codes.
 	 */
 	public static String convertXmlEscapeCharacters(String inputStr) {
-		return inputStr.replaceAll("&", "&amp;").replaceAll("\"", "&quot;").replaceAll("'", "&apos;")
-				.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+		return inputStr.replaceAll(StringConstants.AMPERSAND, AMPERSAND_CODE).replaceAll(
+				StringConstants.QUOTATION_MARK, QUOTATION_MARK_CODE).replaceAll(StringConstants.APOSTROPHE,
+				APOSTROPHE_CODE).replaceAll(StringConstants.LESS_THAN_SIGN, LESS_THAN_SIGN_CODE).replaceAll(
+				StringConstants.GREATER_THAN_SIGN, GREATER_THAN_SIGN_CODE);
+	}
+
+	/**
+	 * Converts the input character into a {@link String}, replacing it with its corresponding XML
+	 * code if necessary, e.g. & --> &amp;
+	 * 
+	 * @param c
+	 *            the character to process
+	 * @return a {@link String} representation of the input character, with XML code replacement if
+	 *         necessary
+	 */
+	public static String escapeForXml(char c) {
+		switch (c) {
+		case StringConstants.AMPERSAND_CHAR:
+			return AMPERSAND_CODE;
+		case StringConstants.APOSTROPHE_CHAR:
+			return APOSTROPHE_CODE;
+		case StringConstants.QUOTATION_MARK_CHAR:
+			return QUOTATION_MARK_CODE;
+		case StringConstants.LESS_THAN_SIGN_CHAR:
+			return LESS_THAN_SIGN_CODE;
+		case StringConstants.GREATER_THAN_SIGN_CHAR:
+			return GREATER_THAN_SIGN_CODE;
+		default:
+			return Character.toString(c);
+		}
+	}
+
+	/**
+	 * Returns an XML header, e.g.
+	 * 
+	 * <pre>
+	 * <?xml version="1.0" encoding="UTF-8"?>
+	 * </pre>
+	 * 
+	 * @param encoding
+	 *            the encoding to specify in the XML header
+	 * @return an XML heading line
+	 */
+	public static String getXmlHeader1_0(CharacterEncoding encoding) {
+		return String.format("<?xml version=\"1.0\" encoding=\"%s\"?>", encoding.name().replace(
+				StringConstants.UNDERSCORE, StringConstants.HYPHEN_MINUS));
 	}
 }
