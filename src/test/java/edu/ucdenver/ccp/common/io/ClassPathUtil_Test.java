a30d22c2f411c880a1c1997e22fed023dc6f5511
Christophe Roeder <croeder@croeder.com>
2011-06-01 19:59:19 +0000

    added code to log class loader name when test is run.

--- src/test/java/edu/ucdenver/ccp/common/io/ClassPathUtil_Test.java
+++ src/test/java/edu/ucdenver/ccp/common/io/ClassPathUtil_Test.java
@@ -4,22 +4,34 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
 
 public class ClassPathUtil_Test {
 	
 	static String OSGI_CLASSLOADER_PREFIX="org.osgi";
+	Logger logger = Logger.getLogger(ClassPathUtil_Test.class);
+	@Before	
+	public void before() {
+		BasicConfigurator.configure();
+	}
 	
 	@Test
 	public  void testFile() throws Exception {
-
-			if (!this.getClass().getClassLoader().getClass().getName().startsWith(OSGI_CLASSLOADER_PREFIX)) {
+		logger.error(this.getClass().getClassLoader().getClass().getName());
+		// in IDE test: sun.misc.Launcher$AppClassLoader
+		// in Hudson OSGI test:
+		
+		if (!this.getClass().getClassLoader().getClass().getName().startsWith(OSGI_CLASSLOADER_PREFIX)) {
 			//org.osgi.framework.BundleReference
 
 			List<String> list = ClassPathUtil.listResourceDirectory(
 				this.getClass(), "test_dir/test_file.txt");
 			Assert.assertTrue(list.get(0).endsWith("test_dir/test_file.txt"));
-			}
+		}
+
 	}
 	
 	/**
@@ -30,14 +42,14 @@ public class ClassPathUtil_Test {
 	@Test
 	public void testDir() throws Exception {
 		if (!this.getClass().getClassLoader().getClass().getName().startsWith(OSGI_CLASSLOADER_PREFIX)) {
-		List<String> list = ClassPathUtil.listResourceDirectory(
-				this.getClass(), "test_dir");
-		String[] classes = { "test_dir/test_file.txt",
-		"test_dir/other_test_file.txt" };
-	
-		for (String c : classes ) {
-			Assert.assertTrue(list.contains(c));
-		}
+			List<String> list = ClassPathUtil.listResourceDirectory(
+					this.getClass(), "test_dir");
+			String[] classes = { "test_dir/test_file.txt",
+			"test_dir/other_test_file.txt" };
+		
+			for (String c : classes ) {
+				Assert.assertTrue(list.contains(c));
+			}
 		}
 	}
 	
@@ -68,6 +80,8 @@ public class ClassPathUtil_Test {
 		
 		String[] classes = { "org/junit/After.class",
 			"org/junit/runners/model/TestClass.class" };
+		
+		System.out.println(this.getClass().getClassLoader().getClass().getName());
 		
 		for (String c : classes ) {
 			Assert.assertTrue(list.contains(c));
