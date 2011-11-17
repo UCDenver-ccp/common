b40ccae78b488469889ae5d35fbb52a673a12062
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2011-11-17 19:43:09 +0000

    Added loadTimeStamp method

--- src/main/java/edu/ucdenver/ccp/common/calendar/CalendarUtil.java
+++ src/main/java/edu/ucdenver/ccp/common/calendar/CalendarUtil.java
@@ -18,8 +18,11 @@
 
 package edu.ucdenver.ccp.common.calendar;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Utility class for handling Calendar operations
@@ -59,6 +62,41 @@ public class CalendarUtil {
 	private static String getDateStamp(GregorianCalendar c, String delimiter) {
 		return String.format("%s%s%s%s%s", c.get(Calendar.MONTH) + 1, delimiter, c.get(Calendar.DAY_OF_MONTH),
 				delimiter, c.get(Calendar.YEAR));
+	}
+
+	/**
+	 * Sun Oct 30 16:07:39 MDT 2011
+	 * 
+	 * @param timeStamp
+	 * @return
+	 */
+	public static Calendar loadTimeStamp(String timeStamp) {
+		String[] toks = timeStamp.split("[\\s:]");
+
+		Map<String, Integer> monthToNumMap = new HashMap<String, Integer>();
+		monthToNumMap.put("sep", 8);
+		monthToNumMap.put("oct", 9);
+		monthToNumMap.put("nov", 10);
+		monthToNumMap.put("dec", 11);
+		monthToNumMap.put("jan", 0);
+		monthToNumMap.put("feb", 1);
+		monthToNumMap.put("mar", 2);
+		monthToNumMap.put("apr", 3);
+		monthToNumMap.put("may", 4);
+		monthToNumMap.put("jun", 5);
+		monthToNumMap.put("jul", 6);
+		monthToNumMap.put("aug", 7);
+
+		String MON = toks[1].toLowerCase();
+		Integer DD = Integer.parseInt(toks[2]);
+		Integer HH = Integer.parseInt(toks[3]);
+		Integer MM = Integer.parseInt(toks[4]);
+		Integer SS = Integer.parseInt(toks[5]);
+		String timeZone = toks[6];
+		Integer YYYY = Integer.parseInt(toks[7]);
+
+		return new GregorianCalendar(YYYY, monthToNumMap.get(MON), DD, HH, MM, SS);
+
 	}
 
 }
