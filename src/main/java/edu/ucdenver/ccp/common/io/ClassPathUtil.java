a7b213cbc625d6961ffb8775c6689f1a756a408a
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2011-12-16 22:41:19 +0000

    Auto-formatted. Added conditional to getResourceStreamFromClasspath that first tries clazz.getResourceAsStream() then tries clazz.getClassLoader().getResourceAsStream() if the first attempt fails.

--- src/main/java/edu/ucdenver/ccp/common/io/ClassPathUtil.java
+++ src/main/java/edu/ucdenver/ccp/common/io/ClassPathUtil.java
@@ -1,23 +1,19 @@
 package edu.ucdenver.ccp.common.io;
 
-
-import org.apache.log4j.Logger;
-
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.File;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.apache.log4j.Logger;
 
 import edu.ucdenver.ccp.common.file.CharacterEncoding;
 import edu.ucdenver.ccp.common.file.FileUtil;
@@ -42,8 +38,10 @@ public class ClassPathUtil {
 	public static InputStream getResourceStreamFromClasspath(Class<?> clazz, String resourceName) {
 		InputStream is = clazz.getResourceAsStream(resourceName);
 		if (is == null) {
-			throw new RuntimeException("resource not found in classpath: " + resourceName + " class is: "
-					+ clazz.getCanonicalName());
+			is = clazz.getClassLoader().getResourceAsStream(resourceName);
+			if (is == null)
+				throw new RuntimeException("resource not found in classpath: " + resourceName + " class is: "
+						+ clazz.getCanonicalName());
 		}
 		return is;
 	}
@@ -74,7 +72,6 @@ public class ClassPathUtil {
 		return file;
 	}
 
-	
 	/**
 	 * Copies a resource (file on the classpath) to the specified directory
 	 * 
@@ -83,15 +80,15 @@ public class ClassPathUtil {
 	 * @param directory
 	 * @throws IOException
 	 */
-	public static File copyClasspathResourceToDirectory(Class<?> clazz, String resourceName, File directory) throws IOException {
+	public static File copyClasspathResourceToDirectory(Class<?> clazz, String resourceName, File directory)
+			throws IOException {
 		return copyClasspathResourceToFile(clazz, resourceName, new File(directory, resourceName));
 	}
-	
-	
+
 	/**
-	 * This function digs through the classpath to find either a file or the contents of a directory,
-	 * whether the file/dir is in a directory or a jar.
-	 * adapted from http://www.uofr.net/~greg/java/get-resource-listing.html
+	 * This function digs through the classpath to find either a file or the contents of a
+	 * directory, whether the file/dir is in a directory or a jar. adapted from
+	 * http://www.uofr.net/~greg/java/get-resource-listing.html
 	 * 
 	 * @param clazz
 	 * @param resourceName
@@ -99,60 +96,51 @@ public class ClassPathUtil {
 	 * @throws IOException
 	 * @throws URISyntaxException
 	 */
-	public static List<String> listResourceDirectory(Class clazz, String resourceName) 
-	throws IOException, URISyntaxException {
-		List<String> list = new ArrayList<String>();
-		
+	public static List<String> listResourceDirectory(Class<?> clazz, String resourceName) throws IOException,
+			URISyntaxException {
+		List<String> resourceNames = new ArrayList<String>();
+
 		URL dirUrl = clazz.getClassLoader().getResource(resourceName);
 		logger.debug("" + dirUrl);
-		
-		// with OSGI this comes back as: 
+
+		// with OSGI this comes back as:
 		// bundleresource://2.fwk1520314875/test_dir/test_file.txt
 		// without:
 		// file:/Users/roederc/subversion_workspace/common/src/test/resources/test_dir/test_file.txt
-		
-		URI uri=null;
+
+		URI uri = null;
 		if (dirUrl != null && dirUrl.getProtocol().equals("file")) {
 			uri = dirUrl.toURI();
-			String[] strings = new File(uri.toString()).list();
-			// input: foo/bar
-			// output: /Users/roederc/foo/bar/baz/f1, /Users/roederc/foo/bar/baz/f2, etc.
-			// should be: foo/bar/baz/f1, foo/bar/baz/f2
-			// find that first part, remove as you iterate
 			String uriString = uri.getPath();
-			String firstPart = uriString.substring(0,uriString.indexOf(resourceName));
+			String resourcePath = uriString.substring(0, uriString.indexOf(resourceName));
 			File dirFile = new File(uri.getPath());
 			if (dirFile.isDirectory()) {
-				// directory
 				for (File f : dirFile.listFiles()) {
-					list.add(f.getPath().substring(firstPart.length()));
-					logger.debug("ClassPathUtil adding from directory:" +f.getPath().substring(firstPart.length()));
+					resourceNames.add(f.getPath().substring(resourcePath.length()));
+					logger.debug("ClassPathUtil adding from directory:" + f.getPath().substring(resourcePath.length()));
 				}
-				return list;
-			}
-			else {
+			} else {
 				// single file
-				list.add(dirFile.getPath().substring(firstPart.length()));
-				logger.debug("ClassPathUtil adding from single file:" + dirFile.getPath().substring(firstPart.length()));
+				resourceNames.add(dirFile.getPath().substring(resourcePath.length()));
+				logger.debug("ClassPathUtil adding from single file:"
+						+ dirFile.getPath().substring(resourcePath.length()));
 			}
-		}
-		
-		if (dirUrl != null && dirUrl.getProtocol().equals("jar")) {
+		} else if (dirUrl != null && dirUrl.getProtocol().equals("jar")) {
 			String jarPath = dirUrl.getPath();
 			// looks like: file:/Users/roederc/junit-4.8.2.jar!/org/junit
-			jarPath = jarPath.substring(5,jarPath.indexOf("!"));
+			jarPath = jarPath.substring(5, jarPath.indexOf("!"));
 			JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
 			Enumeration<JarEntry> entries = jar.entries();
 			while (entries.hasMoreElements()) {
 				String name = entries.nextElement().getName();
 				if (name.startsWith(resourceName) && !name.endsWith("/")) {
 					logger.debug("ClassPathUtil adding from jar:" + name);
-					list.add(name);
+					resourceNames.add(name);
 				}
 			}
 		}
 
-		return list;
+		return resourceNames;
 	}
 
 }
