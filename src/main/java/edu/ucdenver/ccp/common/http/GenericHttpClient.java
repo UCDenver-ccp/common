6ee8ffe5c31c3d02f12f2106cfa200a07004fa77
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2012-08-20 15:58:27 +0000

    organized imports

--- src/main/java/edu/ucdenver/ccp/common/http/GenericHttpClient.java
+++ src/main/java/edu/ucdenver/ccp/common/http/GenericHttpClient.java
@@ -31,7 +31,6 @@ package edu.ucdenver.ccp.common.http;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
